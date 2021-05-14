a4d8636c2593d462167570c5ce9762699d980562
R2002 <saito.3110.0721@gmail.com>
2021-05-14 09:53:31 +0900

    Update config.py

--- app/flask_db_blueprint/config.py
+++ app/flask_db_blueprint/config.py
@@ -8,7 +8,7 @@ cf = {} # 削除しないでください
 # デバッグ表示を有りにする
 cf['DEBUG'] = 1
 
-# BD設定
+# DB設定
 cf['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://{user}:{password}@{host}/{db_name}?charset=utf8'.format(**{
     'user': os.getenv('DB_USER', 'root'),
     'password': os.getenv('DB_PASSWORD', ''),
